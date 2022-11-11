dadfac741c15822e29bbbae7a3e4f27202888767
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-11 10:50:44 -0500

    Solving exercise 3.1 - Minimum

---
+++ Exercises/Ch3_Functions/3.1.Minimum.js
@@ -0,0 +1,11 @@
+/* This script presents a solution to the exercise "Minimum" 
+*  in Chapter 3 of Eloquent JavaScript by Haverbeke.
+*/
+
+function min(x, y)
+{
+    if (x < y) return x;
+    else return y;
+}
+
+console.log(min(0, -10));
